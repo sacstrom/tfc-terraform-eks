2983472f3d9e73f3c0d0a51510dbf7145bb7db8f
Mats Blomdahl <mats.blomdahl@gmail.com>
2025-03-30 23:33:34 +0200

    Correct baseline, no more Terraform Cloud
    - Correct backend to S3
    - Correct Kubernetes version to 1.29
    - Name Nginx Controller by version 1.5.1

--- terraform.tf
+++ terraform.tf
@@ -6,12 +6,10 @@ terraform {
     }
   }
 
-  cloud {
-    organization = "mblomdahl"
-
-    workspaces {
-      name = "tfc-vs4345-k8s"
-    }
+  backend "s3" {
+    bucket = "bamboo02.smithmicro.net-terraform-state"
+    key    = "mb-eks-cluster/terraform.tfstate"
+    region = "eu-north-1"
   }
 
   required_version = ">= 1.3.6"
